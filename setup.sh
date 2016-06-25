26290d87180e59b328745c596a367a38a1eeb63b
Michael Blakeney <mike.blakeney@gmail.com>
2016-06-24 16:17:06 -0800

    auto install vim plugins

--- setup.sh
+++ setup.sh
@@ -31,6 +31,9 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
+# Install all the vim plugins
+vim +PluginInstall +qall
+
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
